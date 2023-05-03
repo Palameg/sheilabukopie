eb52f977f1120f2cf64559becf85a73b89337db9
Palameg <121619952+Palameg@users.noreply.github.com>
2023-05-03 11:09:24 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include("connection/connect.php");  
+error_reporting(0);  
+session_start(); 
+
+?>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">   
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="#">
+    <title>Home</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animsition.min.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet"> </head>
+
+<body class="home">
+    
+        <header id="header" class="header-scroll top-header headrom">
+            <nav class="navbar navbar-dark">
+                <div class="container">
+                    <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
+                    <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/icn.png" alt=""> </a>
+                    <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+                            <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Store <span class="sr-only"></span></a> </li>
+                            
+                           
+							<?php
+						if(empty($_SESSION["user_id"])) // if user is not login
+							{
+								echo '<li class="nav-item"><a href="login.php" class="nav-link active">Login</a> </li>
+							  <li class="nav-item"><a href="registration.php" class="nav-link active">Register</a> </li>';
+							}
+						else
+							{
+
+									
+									echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">My Orders</a> </li>';
+									echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">Logout</a> </li>';
+							}
+
+						?>
+							 
+                        </ul>
+						 
+                    </div>
+                </div>
+            </nav>
+
+        </header>
+
+        <section class="hero bg-image" data-image-src="images/img/two.jpg">
+            <div class="hero-inner">
+                <div class="container text-center hero-text font-white">
+                    <h1> Sheila's Buko Pie </h1>
+                    
+                    <div class="banner-form">
+                        <form class="form-inline">
+                          
+                        </form>
+                    </div>
+                    <div class="steps">
+                        <div class="step-item step1">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pie-chart-fill" viewBox="0 0 16 16">
+                        <g fill="#FFF">
+                          <path d="M15.985 8.5H8.207l-5.5 5.5a8 8 0 0 0 13.277-5.5zM2 13.292A8 8 0 0 1 7.5.015v7.778l-5.5 5.5zM8.5.015V7.5h7.485A8.001 8.001 0 0 0 8.5.015z"/>
+                                </svg>
+                        </g>
+                            <h4><span style="color:white;">1.</span> Choose a Products</h4> </div>
+                
+                        <div class="step-item step2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewbox="0 0 380.721 380.721">
+                                        <g fill="#FFF">
+                                            <path d="M58.727 281.236c.32-5.217.657-10.457 1.319-15.709 1.261-12.525 3.974-25.05 6.733-37.296a543.51 543.51 0 0 1 5.449-17.997c2.463-5.729 4.868-11.433 7.25-17.01 5.438-10.898 11.491-21.07 18.724-29.593 1.737-2.19 3.427-4.328 5.095-6.46 1.912-1.894 3.805-3.747 5.676-5.588 3.863-3.509 7.221-7.273 11.107-10.091 7.686-5.711 14.529-11.137 21.477-14.506 6.698-3.724 12.455-6.982 17.631-8.812 10.125-4.084 15.883-6.141 15.883-6.141s-4.915 3.893-13.502 10.207c-4.449 2.917-9.114 7.488-14.721 12.147-5.803 4.461-11.107 10.84-17.358 16.992-3.149 3.114-5.588 7.064-8.551 10.684-1.452 1.83-2.928 3.712-4.427 5.6a1225.858 1225.858 0 0 1-3.84 6.286c-5.537 8.208-9.673 17.858-13.995 27.664-1.748 5.1-3.566 10.283-5.391 15.534a371.593 371.593 0 0 1-4.16 16.476c-2.266 11.271-4.502 22.761-5.438 34.612-.68 4.287-1.022 8.633-1.383 12.979 94 .023 166.775.069 268.589.069.337-4.462.534-8.97.534-13.536 0-85.746-62.509-156.352-142.875-165.705 5.17-4.869 8.436-11.758 8.436-19.433-.023-14.692-11.921-26.612-26.631-26.612-14.715 0-26.652 11.92-26.652 26.642 0 7.668 3.265 14.558 8.464 19.426-80.396 9.353-142.869 79.96-142.869 165.706 0 4.543.168 9.027.5 13.467 9.935-.002 19.526-.002 28.926-.002zM0 291.135h380.721v33.59H0z" /> </g>
+                                    </svg>
+                                </g>
+                            <h4><span style="color:white;">2.</span> Reservation</h4> </div>
+              
+                        <div class="step-item step3">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="512" height="512"  class="bi bi-shop" viewBox="0 0 12 18">
+                           <g fill="#FFF">
+                        <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zM4 15h3v-5H4v5zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3zm3 0h-2v3h2v-3z"/>
+                        </g>
+                    </svg>
+                            <h4><span style="color:white;"> 3. </span> Self-Pickup </h4> </div>
+                   
+                    </div>
+             
+                </div>
+            </div>
+      
+        </section>
+      
+      
+	  
+	
+     
+        <section class="popular">
+            <div class="container">
+                <div class="title text-xs-center m-b-30">
+                    <h2>BEST SELLER</h2>
+                    <p class="lead"> These are the three best-selling pies to choose form. Pick your Favorite!  </p>
+                </div>
+                <div class="row">
+						<?php 					
+						$query_res= mysqli_query($db,"select * from dishes LIMIT 3"); 
+                                while($r=mysqli_fetch_array($query_res))
+                                {
+                                        
+                                    echo '  <div class="col-xs-12 col-sm-6 col-md-4 food-item">
+                                            <div class="food-item-wrap">
+                                                <div class="figure-wrap bg-image" data-image-src="admin/Res_img/dishes/'.$r['img'].'"></div>
+                                                <div class="content">
+                                                    <h5><a href="dishes.php?res_id='.$r['rs_id'].'">'.$r['title'].'</a></h5>
+                                                    <div class="product-name">'.$r['slogan'].'</div>
+                                                    <div class="price-btn-block"> <span class="price">₱'.$r['price'].'</span> <a href="dishes.php?res_id='.$r['rs_id'].'" class="btn theme-btn-dash pull-right">Reserve Now</a> </div>
+                                                </div>
+                                                
+                                            </div>
+                                    </div>';                                      
+                                }	
+						?>
+                </div>
+            </div>
+        </section>
+ 
+        <section class="how-it-works">
+            <div class="container">
+                <div class="text-xs-center">
+                    <h2>Easy to Order</h2>
+                    <div class="row how-it-works-solution">
+                        <div class="col-xs-12 col-sm-12 col-md-4 how-it-works-steps white-txt col1">
+                            <div class="how-it-works-wrap">
+                                <div class="step step-1">
+                                    <div class="icon" data-step="1">
+                                        <<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pie-chart-fill" viewBox="0 0 18 16">
+                                            <g fill="#FFF">
+                                            <path d="M15.985 8.5H8.207l-5.5 5.5a8 8 0 0 0 13.277-5.5zM2 13.292A8 8 0 0 1 7.5.015v7.778l-5.5 5.5zM8.5.015V7.5h7.485A8.001 8.001 0 0 0 8.5.015z"/>
+                                                    </svg>
+                                            </g>
+                                    </div>
+                                    <h3>Choose a Product</h3>
+                                    <p>We've got you covered with variety of PIES that is fit for your likeness!</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-4 how-it-works-steps white-txt col2">
+                            <div class="step step-2">
+                                <div class="icon" data-step="2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewbox="0 0 380.721 380.721">
+                                        <g fill="#FFF">
+                                            <path d="M58.727 281.236c.32-5.217.657-10.457 1.319-15.709 1.261-12.525 3.974-25.05 6.733-37.296a543.51 543.51 0 0 1 5.449-17.997c2.463-5.729 4.868-11.433 7.25-17.01 5.438-10.898 11.491-21.07 18.724-29.593 1.737-2.19 3.427-4.328 5.095-6.46 1.912-1.894 3.805-3.747 5.676-5.588 3.863-3.509 7.221-7.273 11.107-10.091 7.686-5.711 14.529-11.137 21.477-14.506 6.698-3.724 12.455-6.982 17.631-8.812 10.125-4.084 15.883-6.141 15.883-6.141s-4.915 3.893-13.502 10.207c-4.449 2.917-9.114 7.488-14.721 12.147-5.803 4.461-11.107 10.84-17.358 16.992-3.149 3.114-5.588 7.064-8.551 10.684-1.452 1.83-2.928 3.712-4.427 5.6a1225.858 1225.858 0 0 1-3.84 6.286c-5.537 8.208-9.673 17.858-13.995 27.664-1.748 5.1-3.566 10.283-5.391 15.534a371.593 371.593 0 0 1-4.16 16.476c-2.266 11.271-4.502 22.761-5.438 34.612-.68 4.287-1.022 8.633-1.383 12.979 94 .023 166.775.069 268.589.069.337-4.462.534-8.97.534-13.536 0-85.746-62.509-156.352-142.875-165.705 5.17-4.869 8.436-11.758 8.436-19.433-.023-14.692-11.921-26.612-26.631-26.612-14.715 0-26.652 11.92-26.652 26.642 0 7.668 3.265 14.558 8.464 19.426-80.396 9.353-142.869 79.96-142.869 165.706 0 4.543.168 9.027.5 13.467 9.935-.002 19.526-.002 28.926-.002zM0 291.135h380.721v33.59H0z" /> </g>
+                                    </svg>
+                                </div>
+                                <h3>Make a Reservation</h3>
+                                <p>Make a Reservation depending on your schedule.</p>
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-12 col-md-4 how-it-works-steps white-txt col3">
+                            <div class="step step-3">
+                                <div class="icon" data-step="3">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="512" height="512"  class="bi bi-shop" viewBox="0 0 12 18">
+                                    <g fill="#FFF">
+                                    <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zM4 15h3v-5H4v5zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3zm3 0h-2v3h2v-3z"/>
+                                    </g>
+                                </svg>
+                            </div>
+                                <h3> Self-Pick up </h3>
+                                <p>You may pick up your order at our shop when the procedure is completed.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12 text-center">
+                        <p class="pay-info">GCASH AND CASH PAYMENT</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="featured-restaurants">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="title-block pull-left">
+                            <h4>Store</h4> </div>
+                    </div>
+                    <div class="col-sm-8">
+                        <div class="restaurants-filter pull-right">
+                            <nav class="primary pull-left">
+                                <ul>
+                                    <li><a href="#" class="selected" data-filter="*">all</a> </li>
+									<?php 
+									$res= mysqli_query($db,"select * from res_category");
+									      while($row=mysqli_fetch_array($res))
+										  {
+											echo '<li><a href="#" data-filter=".'.$row['c_name'].'"> '.$row['c_name'].'</a> </li>';
+										  }
+									?>
+                                   
+                                </ul>
+                            </nav>
+                        </div>
+          
+                    </div>
+                </div>
+    
+                <div class="row">
+                    <div class="restaurant-listing">
+                        
+						
+						<?php  
+						$ress= mysqli_query($db,"select * from restaurant");  
+									      while($rows=mysqli_fetch_array($ress))
+										  {
+													
+													$query= mysqli_query($db,"select * from res_category where c_id='".$rows['c_id']."' ");
+													 $rowss=mysqli_fetch_array($query);
+						
+													 echo ' <div class="col-xs-12 col-sm-12 col-md-6 single-restaurant all '.$rowss['c_name'].'">
+														<div class="restaurant-wrap">
+															<div class="row">
+																<div class="col-xs-12 col-sm-3 col-md-12 col-lg-3 text-xs-center">
+																	<a class="restaurant-logo" href="dishes.php?res_id='.$rows['rs_id'].'" > <img src="admin/Res_img/'.$rows['image'].'" alt="Restaurant logo"> </a>
+																</div>
+													
+																<div class="col-xs-12 col-sm-9 col-md-12 col-lg-9">
+																	<h5><a href="dishes.php?res_id='.$rows['rs_id'].'" >'.$rows['title'].'</a></h5> <span>'.$rows['address'].'</span>
+																</div>
+													
+															</div>
+												
+														</div>
+												
+													</div>';
+										  }
+						
+						
+						?>
+						
+							
+						
+					
+                    </div>
+                </div>
+     
+               
+            </div>
+        </section>
+        
+      
+        <footer class="footer">
+            <div class="container">
+                
+          
+                <div class="bottom-footer">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-3 payment-options color-black">
+                            <h5>Payment Options</h5>
+                            <ul>
+                                <li>
+                                    <a href="#"> <img src="images/gcash.png" alt="Gcash"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/cash.png" alt="Cash"> </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-xs-12 col-sm-4 address color-black">
+                                    <h5>Address</h5>
+                                    <p>Sheila Rada</p>
+                                    <h5>Phone Number: 094993151879</a></h5> </div>
+                                <div class="col-xs-12 col-sm-5 additional-info color-black">
+                                    <h5>Addition informations</h5>
+                                   <p>Join thousands of other restaurants who benefit from having partnered with us.</p>
+                                </div>
+                    </div>
+                </div>
+          
+            </div>
+        </footer>
+    
+   
+                    
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/animsition.min.js"></script>
+    <script src="js/bootstrap-slider.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/headroom.js"></script>
+    <script src="js/foodpicky.min.js"></script>
+</body>
+
+</html>
